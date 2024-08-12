818f2cb0bb4f64adb4ad893fe3d8a4d1d5fb69ca
Jeff Balbalosa <cyjeffb@yahoo.com>
2024-08-12 11:07:38 +0900

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int num = 10;
+double price = 19.95;
+string name = "Frank";
+
+//Interpolation
+Console.WriteLine($"The number is {num}");
+
+// String concatination
+Console.WriteLine("The number is " + num);
+
+//String formatting
+Console.WriteLine("The number is {0}, and the price is {1}, and my name is {2}", num,price, name);
+Console.ReadKey();
